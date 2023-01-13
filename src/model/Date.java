4ef6ee5c8599e5981f66196a70830fb8f38a6540
lucasTrswl <lucasturschwell@hotmail.com>
2023-01-13 16:34:06 +0100

    final version avec commentaires

--- src/model/Date.java
+++ src/model/Date.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.text.ParseException;
 import java.util.ArrayList;
 
+// constructeur de la classe Date qui initialise les variables d'année, mois et jour
 public class Date {
     private int Annee;
     private int Mois;
@@ -16,14 +17,19 @@ public class Date {
       this.Jour = jour;
     }
     
+    // methode qui retourne l'année
+
     public int getAnnee() {
         return Annee;
     }
+
+    // methode qui retourne le nom du contact associé à cette date
 
     public String getContactName() {
         return contactName;
     }
 
+     // methode qui set le nom du contact associé à cette date en recherchant dans le fichier csv
     public void setContactName() {
         try {
             ArrayList<Contact> list = Contact.lister();
@@ -37,33 +43,40 @@ public class Date {
         }
     }
 
+    // methode qui set l'année
+
     public void setAnnee(int annee) {
         Annee = annee;
     }
 
 
+    // methode qui retourne le mois
 
     public int getMois() {
         return Mois;
     }
 
 
-
+    // methode qui set le mois
     public void setMois(int mois) {
         Mois = mois;
     }
 
 
+    // methode qui retourne le jour
 
     public int getJour() {
         return Jour;
     }
 
 
+    // methode qui set le jour
 
     public void setJour(int jour) {
         Jour = jour;
     }
+
+    // methode qui compte le nombre de chiffres dans un nombre
 
     public int countDig(int n)  {  
         int count = 0;  
@@ -74,6 +87,8 @@ public class Date {
         }  
         return count;  
     }  
+
+    // Methode toString qui retourne une chaine de caractère qui contient le nom du contact associé à cette date et la date
 
     @Override
     public String toString() {
